3f572094d3480e17a36f1a0208588b2272263bd2
juandark45 <152802924+juandark45@users.noreply.github.com>
2025-04-03 11:19:35 -0500

    Trabajo 3/04
    Empezando un menu con tuplas y que en las opciones pueda aplicar zip, sorted y reverse

---
+++ Python/Equipo.py
@@ -0,0 +1,8 @@
+def Equipo_pokemon():
+    print ("1. Desea crear un equipo?")
+    print ("2. Quiere que este ordenado a la inversa?")
+    print ("3. Quiere ver en que posicion va cada miembro")
+    print ("4. Quiere ordenarlos de otra forma")
+    opcion = int(input())
+
+    return opcion
