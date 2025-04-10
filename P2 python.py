e11910ede3bbad57de257b2d53ae63a2d3243002
juandark45 <152802924+juandark45@users.noreply.github.com>
2025-04-10 11:19:07 -0500

    P2 python
    primeros pasos del parcial

---
+++ P2 python.py
@@ -0,0 +1,32 @@
+#Empresa de logitica 
+from optparse import Option
+
+
+def Menu_de_los_clientes():
+    print ("Bienvenido a la empresa de logistica")
+    print ("1. Ingresar un nuevo cliente")
+    print ("2. Modificar un cliente existente")
+    print ("3. Se desesa eliminar un cliente")
+    print ("4. Mostrar la informacion de todos los clientes")
+    print ("5. Mostrar la informacion de un cliente especifico")
+    print ("6. Salir")
+
+    opcion = int(input("Escoja alguna opcion"))
+
+    return opcion 
+
+def Ingrseo_cliente():
+
+    if Option == 1:
+
+        print ("Ingrese el nombre del cliente")
+        print ("Ingrese el apellido del cliente")
+        print ("Ingrese el telefono del cliente")
+        print ("ingrese la direccion del cliente")
+        print ("ingrese la identificacion del cliente")
+
+def Menu_de_Bienes():
+    print ("Desea ver los bienes disponibles")
+    print ("Desea registrar un articulo nuevo?")
+    print ("Desea modificar un articulo ya existente")
+    print ("Desea ver los bienes filtrados en disponibles y no disponibles")
